c2cdcaa4e244122feaf536e47889b1aad9f33952
ACHIVX <achivx@proton.me>
2024-09-04 17:58:56 +0600

    add mainnet config and parameters template

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.26",
+  networks: {
+    mainnet: {
+      url: vars.get("MAINNET_GATEWAY_URL"),
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+  },
+  etherscan: {
+    apiKey: vars.get("ETHERSCAN_API_KEY"),
+  },
 };
