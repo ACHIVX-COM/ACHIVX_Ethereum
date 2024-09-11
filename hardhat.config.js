2b0637fb52b8832d992227893cf2f12d6c411734
ACHIVX <achivx@proton.me>
2024-09-11 18:55:45 +0600

    add more network configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,32 @@ module.exports = {
       url: vars.get("MAINNET_GATEWAY_URL"),
       accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
     },
-    arbitrum: {
+    arbitrumOne: {
       url: "https://arb1.arbitrum.io/rpc",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    avalancheMain: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    polygonMain: {
+      url: "https://polygon-bor-rpc.publicnode.com",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    optimismMain: {
+      url: "https://optimism-rpc.publicnode.com",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    baseMain: {
+      url: "https://base-rpc.publicnode.com",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    bnbSmartChainMain: {
+      url: "https://bsc-rpc.publicnode.com",
+      accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
+    },
+    opBnbMain: {
+      url: "https://opbnb-rpc.publicnode.com",
       accounts: [vars.get("MAINNET_ACCOUNT_PRIVATE_KEY")],
     },
   },
